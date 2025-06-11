a8758f0b2dd4b298132468fc6e734971c5566233
Dorcas <ayanfedorcas2017@gmail.com>
2025-06-11 18:08:46 +0100

    edit

--- src/pages/IntroToSignup.jsx
+++ src/pages/IntroToSignup.jsx
@@ -60,7 +60,7 @@ const IntroToSignup = () => {
     <>
       <div className="h-full w-full absolute">
         <div
-          className={`fixed h-full w-full bg-[url('${imageURL.bgURL}')] bg-cover bg-center filter blur-sm`}
+          className={`fixed h-full w-full bg-[url('https://assets.nflxext.com/ffe/siteui/vlv3/f841d4c7-10e1-40af-bcae-07a3f8dc141a/f6d7434e-d6de-4185-a6d4-c77a2d08737b/US-en-20220502-popsignuptwoweeks-perspective_alpha_website_medium.jpg')] bg-cover bg-center filter blur-sm`}
         ></div>
 
         <div className="w-full h-full bg-black/60 bg-gradient-to-t from-black fixed top-0 left-0"></div>
